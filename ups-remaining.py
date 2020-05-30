42fd183713fd302832b7df45ebc8586b646ab9b7
vom <vom@ice.internal.burn.net>
2020-05-30 12:25:46 -0400

    Added ups-remaining.py script.

---
+++ ups-remaining.py
@@ -0,0 +1,51 @@
+#!/usr/bin/python
+
+import subprocess
+import time
+import datetime
+import os
+import sys
+from termcolor import colored
+
+# List of UPSes to monitor.  <ups_name>@<hostname> format. Or just <ups_name> is for a locally connected unit.
+upses = ('ups', 'ups@fileserver1', 'ups@testbox')
+
+os.system('clear')
+
+try:
+	refresh = 15
+
+	while True:
+		print
+		print "Refreshing every", refresh, "seconds"
+		print "==============================="
+
+		now = datetime.datetime.now().strftime("%a %B %d %Y %H:%M:%S")
+
+		print now
+		print
+
+		for ups in upses:
+			runtime_command	= 'upsc ' + ups + ' battery.runtime 2>/dev/null'
+			charge_command 	= 'upsc ' + ups + ' battery.charge 2>/dev/null'
+
+			try:
+				remaining_seconds = subprocess.check_output(runtime_command, shell=True)
+				charge = subprocess.check_output(charge_command, shell=True)
+			except subprocess.CalledProcessError as e:
+    				print "Error with ups", ups
+				continue
+
+			remaining_seconds = int(remaining_seconds, 10)
+			hms = str(datetime.timedelta(seconds=remaining_seconds))
+
+			print ('%s:' % ups)
+			print "-------------------------------"
+			print "Runtime remaining:", colored(hms, 'cyan')
+			print "Battery charge % :", colored(charge, 'yellow')
+
+		time.sleep(refresh)
+		os.system('clear')
+
+except KeyboardInterrupt:
+	sys.exit(0) # or 1, or whatever
